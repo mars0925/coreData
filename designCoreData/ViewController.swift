45200443c72fa92a88d600dd15424d9cc928ab1d
張宮寶 <mars09250148@gmail.com>
2019-07-01 09:20:26 +0800

    Initial Commit

---
+++ designCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  designCoreData
+//
+//  Created by Mars on 2019/7/1.
+//  Copyright © 2019 drs24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
